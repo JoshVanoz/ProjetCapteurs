121e0adacf89b764c5de0c20b8b63e0c83e93acd
Tanguy <sheik37.v@gmail.com>
2018-03-20 14:31:08 +0100

    ajout de l'affichage des capteurs sur la map de leur parterre

--- NotreApli/Web/Apply/models.py
+++ NotreApli/Web/Apply/models.py
@@ -426,6 +426,9 @@ def get_capteurs():
 def get_capteur(id):
     return Capteur.query.get(id)
 
+def get_capteurs_parterre(id):
+    return Capteur.query.filter(Capteur.parterre_id==id).one()
+
 def get_plante(id):
     return TypePlante.query.get(id)
 
